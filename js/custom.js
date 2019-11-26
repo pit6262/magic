60a6e3ac52b9ebc8137858df55358bc2b0864f5e
Pit <orel-1993@yandex.com>
2019-11-26 17:13:57 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -8,9 +8,63 @@ $(function(){
 	/* Burger */
 	/* ---------------------------------------------- */
 
-	$(".toggle-menu").on('click',function(){
-		$(this).find(".burger__icon").toggleClass("open");
-	});
+	function openUserMenu() {	
+		$(".toggle-menu").on('click',function(){
+			$('.nav-menu-container').toggleClass("is-open");
+			$('body').addClass('lock');
+		});
+
+		$(document).on('click', '.nav-menu-container, .nav-menu-container .container', function(e){
+			if (e.target !== this) 
+				return;
+			$('.nav-menu-container').removeClass('is-open');
+			$('body').removeClass('lock');
+			return false;
+		});
+
+		$(".nav-menu__close").on('click',function(){
+			$('.nav-menu-container').removeClass("is-open");
+			$('body').removeClass('lock');
+		});
+	}
+
+	openUserMenu();
+
+	function openMobileMenu() {	
+		$('.m-menu li:not(.m-menu__item).dropdown > a').on('click', function(){
+			$(this).closest('.m-sub-menu').addClass('is-sub-menu-open')
+			$(this).parent().addClass('is-active')
+
+			return false;
+		});
+
+		$('li:not(.m-menu__item).dropdown > .m-sub-menu > .m-sub-menu__header .m-menu-back').on('click', function(){
+			$(this).closest('li.dropdown').removeClass('is-active')
+			$(this).closest('li.dropdown').closest('.m-sub-menu').removeClass('is-sub-menu-open')
+			return false;
+		});
+
+		$('.m-menu .m-menu__item.dropdown > a').on('click', function(){
+			$('.nav-menu').addClass('is-menu-open');
+			$(this).parent().addClass('is-active');
+			return false;
+		});
+
+		$('.m-menu__item > .m-sub-menu > .m-sub-menu__header .m-menu-back').on('click', function(){
+			$('.nav-menu').removeClass('is-menu-open');
+			$(this).parents('li.dropdown').removeClass('is-active')
+			return false;
+		});
+	}
+	openMobileMenu();
+
+	function openMobileLoginForm() {	
+		$('.open-login-form').on('click',function(){
+			$('.nav-menu__form').toggleClass('hidden-md')
+		})
+	}
+
+	openMobileLoginForm()
 
 
 	/* Tabs */
@@ -42,6 +96,7 @@ $(function(){
 			prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
 			nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>',
 			fade: true,
+			adaptiveHeight: true,
 		});
 	};
 
@@ -50,8 +105,32 @@ $(function(){
 		$('.brands-slider').slick({
 			arrows: false,
 			slidesToShow: 6,
-			slidesToScroll: 6,
-			infinite: false,
+			slidesToScroll: 1,
+
+			autoplay: true,
+
+			responsive: [{
+				breakpoint: 1199, 
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 1,
+				} 
+			},
+			{
+				breakpoint: 767, 
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				} 
+			},
+			{
+				breakpoint: 574, 
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				} 
+			}]
+
 		});
 	};
 
@@ -64,7 +143,25 @@ $(function(){
 			appendArrows: '.action-slider-pagination',
 			prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
 			nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>',
-			
+			responsive: [{
+				breakpoint: 991, 
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				}
+			},{
+				breakpoint: 767, 
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				} 
+			},{
+				breakpoint: 574, 
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				} 
+			}]
 		});
 	};
 	if($('.new-slider').length){
@@ -75,7 +172,25 @@ $(function(){
 			appendArrows: '.new-slider-pagination',
 			prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
 			nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>',
-			
+			responsive: [{
+				breakpoint: 991, 
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				}
+			},{
+				breakpoint: 767, 
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				} 
+			},{
+				breakpoint: 574, 
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				} 
+			}]
 		});
 	};
 	if($('.best-slider').length){
@@ -87,7 +202,25 @@ $(function(){
 			appendArrows: '.best-slider-pagination',
 			prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
 			nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>',
-			
+			responsive: [{
+				breakpoint: 991, 
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				}
+			},{
+				breakpoint: 767, 
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				} 
+			},{
+				breakpoint: 574, 
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				} 
+			}]
 		});
 	};
 
@@ -97,6 +230,7 @@ $(function(){
 			slidesToShow: 1,
 			slidesToScroll: 1,
 			infinite: false,
+			fade: true,
 			appendArrows: '.news-slider-pagination',
 			prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
 			nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>',
@@ -110,6 +244,7 @@ $(function(){
 			slidesToShow: 1,
 			slidesToScroll: 1,
 			infinite: false,
+			fade: true,
 			appendArrows: '.card-inner-slider-pagination',
 			prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></button>',
 			nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right"></use></svg></button>',
